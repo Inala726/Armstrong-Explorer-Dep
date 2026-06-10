4dd5e7ceac1ec4476bd34aba035189bc57d4ea20
Zion Omogbeme <omogbemezion@gmail.com>
2026-06-10 17:57:45 +0100

    changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://amelie-aerogenic-contractibly.ngrok-free.dev/api';
 
 const ACCESS_TOKEN_KEY = 'access_token';
 const REFRESH_TOKEN_KEY = 'refresh_token';
